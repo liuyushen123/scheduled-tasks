41dc6848a1b76d822ac0dbc54f5f843f215c4cea
Liu yuchen <liuyuchen@Esdeathe-4.local>
2026-03-22 12:43:46 -0600

    update script

--- main.py
+++ main.py
@@ -7,13 +7,11 @@ from smtplib import SMTP
 
 import pandas as pd
 
-FILE_PATH = f"letter_templates/letter_{random.randint(1, 10)}.txt"
-
 today = (dt.datetime.now().month, dt.datetime.now().day, dt.datetime.now().year)
 current_month = today[0]
 current_day = today[1]
 current_year = today[2]
-
+print(f"Script running at {today}")
 my_email = os.environ["MY_EMAIL"]
 my_password = os.environ["MY_PASSWORD"]
 
@@ -54,18 +52,26 @@ birthday_data = pd.read_csv("birthdays.csv")
 todays_birthday = birthday_data[
     (birthday_data["month"] == current_month) & (birthday_data["day"] == current_day)
 ]
-
-for index, person in todays_birthday.iterrows():
-    try:
-        with open(FILE_PATH) as file:
-            letter_template = file.read()
-            letter_template = (
-                letter_template.replace("[NAME]", person["name"])
-                .replace("Angela", "Best Regards\nYuchen")
-                .replace("[AGE]", calculate_person_age(current_year - person["year"]))
-            )
-            print(letter_template)
-            send_email(person["email"], person["name"], letter_template)
-    except Exception as e:
-        print(f"Error: {e}")
-        raise
+print(f"Found {len(todays_birthday)} birthday(s) today")
+if not todays_birthday.empty:
+    for index, person in todays_birthday.iterrows():
+        try:
+            with open(f"letter_templates/letter_{random.randint(1, 10)}.txt") as file:
+                letter_template = file.read()
+                letter_template = (
+                    letter_template.replace("[NAME]", person["name"])
+                    .replace("Angela", "Best Regards\nYuchen")
+                    .replace(
+                        "[AGE]", calculate_person_age(current_year - person["year"])
+                    )
+                )
+                print(f"Sending birthday email to {person['name']} ({person['email']})")
+                send_email(person["email"], person["name"], letter_template)
+                print(f"Email sent to {person['name']} ✅")
+        except Exception as e:
+            print(f"Failed to send email to {person['name']}: {e}")
+            continue
+else:
+    print(
+        f"Today is {current_year}/{current_month}/{current_day}: No birthdays today 🎂"
+    )
